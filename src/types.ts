3f470c0793367dea55d2962c31f4c32c176725a7
Mack Ignacio <mack_ignacio@yahoo.com>
2023-08-03 01:31:25 +0800

    feat(types): :sparkles: add types.ts

---
+++ src/types.ts
@@ -0,0 +1,62 @@
+import { Context, RequestObject, ResponseObject } from "./interfaces";
+
+/**
+ * Type of request method
+ */
+export type RequestMethod = "GET" | "POST" | "DELETE" | "OPTIONS" | "PUT" | "PATCH";
+
+/**
+ *
+ */
+export type RouteMethodObjectCallback = {
+  callback: RouteCallback;
+  middlewares: Middlewares[];
+};
+
+/**
+ *  A record of method name and its callback functions
+ * */
+export type RouteMethodObject = {
+  /**
+   * A function that will be executed once the 'path-name' is the same with the request path
+   *
+   * ```
+   * {
+   *   GET: ({ req, body, params, query }) => {
+   *       return 'Hello, world'; // You can also return a JSON object
+   *   }
+   * }
+   * ```
+   */
+  [P in RequestMethod]: RouteCallback | RouteMethodObjectCallback;
+};
+
+/**
+ *
+ */
+export type NextFunction = (error?: any) => Promise<void> | void;
+
+/**
+ *
+ */
+export type RouteCallback = (ctx: Context) => Promise<unknown>;
+
+/**
+ *
+ */
+export type LegacyMiddleware = (req: RequestObject, res: ResponseObject, next: NextFunction) => void;
+
+/**
+ *
+ */
+export type LegacyMiddlewareError = (error: any, req: RequestObject, res: ResponseObject, next: NextFunction) => void;
+
+/**
+ *
+ */
+export type MayaMiddleware = (context: Context, next: NextFunction) => void;
+
+/**
+ *
+ */
+export type Middlewares = LegacyMiddleware | LegacyMiddlewareError | MayaMiddleware;
