ae7736b637d3e2f95e3185862fdd7dd241e20fb6
Mack Ignacio <mack_ignacio@yahoo.com>
2023-08-04 21:27:13 +0800

    refactor(regex): :recycle: add notParam function in regex module

--- src/regex.ts
+++ src/regex.ts
@@ -2,8 +2,11 @@ function base(val: string) {
   return new RegExp(`^${val}$`);
 }
 
+function notParam(val: string) {
+  return `(?:\\/(${val}))`;
+}
+
 function regex(url: string, full: boolean = false): RegExp | string {
-  const notParam = (val: string) => `(?:\\/(${val}))`;
   const isParam = (val: string) => `(?:\\/(?<${val}>[\\w\\-]+?))`;
   const pattern = url
     .split("/")
