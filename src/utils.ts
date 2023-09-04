ecf9189814960a96132fa8a5b408a0bca1383fbf
Mack Ignacio <mack_ignacio@yahoo.com>
2023-09-04 15:59:23 +0800

    refactor(utils): :sparkles: add parseParams

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,25 @@ export function statusCodeFactory(res: ResponseObject) {
 }
 
 export const pathUrl = (url: string) => url.replace(/(^\/+)|(\/+$)/g, "");
+
+export const parseParams = (params: { [x: string]: string }) => {
+  let parsed: { [x: string]: string | number | boolean } = {};
+
+  Object.keys(params).forEach((param) => {
+    const value = params[param];
+
+    if (!isNaN(+value)) {
+      parsed[param] = +value;
+      return;
+    }
+
+    if (value === "true" || value === "false") {
+      parsed[param] = value === "true";
+      return;
+    }
+
+    parsed[param] = value;
+  });
+
+  return parsed;
+};
